7fb1bda7fbf82e159fa73dec15aa605d09be7434
liaoxt <liaoxt@chint.com>
2019-02-02 16:51:33 +0800

    修改端口号

--- src/common/js/config.js
+++ src/common/js/config.js
@@ -1,4 +1,4 @@
-export const HOST = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'http://106.12.92.32:3000'
+export const HOST = process.env.NODE_ENV === 'development' ? 'http://localhost' : 'http://106.12.92.32'
 export const ERR_OK = 200
 
 export const playMode = {
